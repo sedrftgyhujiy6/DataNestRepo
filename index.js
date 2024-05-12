847c6bb2994834fb2bb348b1e52f0358ecdf67c9
sedrftgyhujiy6 <js1taiany16@hotmail.com>
2024-05-12 10:53:29 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function capitalizeWords(str) {
-  return str
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+function combinationSum3(k, n) {
+  const result = [];
+  backtrack([], 1, k, n);
+  return result;
+  function backtrack(combination, start, k, n) {
+    if (n === 0 && k === 0) {
+      result.push([...combination]);
+      return;
+    }
+    if (n < 0 || k === 0) return;
+    for (let i = start; i <= 9; i++) {
+      combination.push(i);
+      backtrack(combination, i + 1, k - 1, n - i);
+      combination.pop();
+    }
+  }
 }
